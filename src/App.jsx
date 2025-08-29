b35018431c2cc1d27b46463b742312dda85b100b
zabaoit <nguyengiabao8407@gmail.com>
2025-08-29 15:10:19 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Toaster />
-      <BrowserRouter>
+      <BrowserRouter basename="/portfolio">
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
